8e341315bc8fb5da6164f7eb98467991aa882f2a
陽希 吉田 <k025g1002@g.neec.ac.jp>
2026-04-16 16:25:45 +0900

    プロジェクト ファイルを追加します。

---
+++ Source/EN01_01Soukoban/EN1_01.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class EN01_01SOUKOBAN_API EN1_01
+{
+public:
+	EN1_01();
+	~EN1_01();
+};
